723f112c694701a33432e941e0b79e3b29169989
sumitdaroch <sumitkumardaroch@gmail.com>
2020-03-07 19:37:48 +0530

    Add files via upload

---
+++ dev.php
@@ -0,0 +1,57 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+  crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</head>
+<a href="#" data-toggle="modal" data-target="#developers">Developers</a>
+
+<div class="modal fade title1" id="developers">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
+      </div>
+	  
+      <div class="modal-body">
+        <p>
+		<div class="row">
+		<div class="col-md-4">
+		 <img src="image/sumit_image.jpg" width=100 height=100 alt="Sumit Kumar Daroch" class="img-rounded">
+		 </div>
+		 <div class="col-md-5">
+		<a href="https://www.linkedin.com/in/anshul-sharma-836a4316b/" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on linkedin">Anshul Sharma</a>
+		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 8894565130</h4>
+		<h4 style="font-family:'typo' ">sharmabjnth@gmail.com</h4>
+		<h4 style="font-family:'typo' ">NIT Hamirpur,Himachal Pradesh</h4></div></div>
+		</p>
+    <hr>
+       <p>
+    <div class="row">
+    <div class="col-md-4">
+     <img src="image/Rishabh_image.jpeg" width=100 height=100 alt="Rishabh Jain" class="img-rounded">
+     </div>
+     <div class="col-md-5">
+    <a href="#" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on linkedin">Preekshit Chandel</a>
+    <h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 8894091845</h4>
+    <h4 style="font-family:'typo' ">preekshitchandel98@gmail.com</h4>
+    <h4 style="font-family:'typo' ">NIT Hamirpur,Himachal Pradesh</h4></div></div>
+    </p>
+      </div>
+    
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
